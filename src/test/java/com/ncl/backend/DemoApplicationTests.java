b16e50dfdf47f95e0981906f2fda2a07c6644c2a
Nhatcao <mitomdasem@gmail.com>
2020-11-25 17:31:46 +0700

    first commit

---
+++ src/test/java/com/ncl/backend/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncl.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
