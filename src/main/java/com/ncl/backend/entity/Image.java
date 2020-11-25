c9c12870b7b639cab769ccc2500f09b975a109fa
Nhatcao <mitomdasem@gmail.com>
2020-11-25 17:59:06 +0700

    init entity

--- src/main/java/com/ncl/backend/entity/Image.java
+++ src/main/java/com/ncl/backend/entity/Image.java
@@ -1,2 +1,38 @@
-package com.ncl.backend.entity;public class Image {
+package com.ncl.backend.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Data
+@Table(name = "image")
+public class Image implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "title", nullable = true)
+    private String title;
+
+    @Column(name = "brief", nullable = true)
+    private String brief;
+
+    @Column(name = "short_description", nullable = true)
+    private String shortDescription;
+
+    @Column(name = "description", nullable = true)
+    private String description;
+
+    @Column(name = "type", nullable = true)
+    private String type;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", referencedColumnName = "id", nullable = false)
+    private Post post;
+
 }
